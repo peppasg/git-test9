5356e45a83a8d6ed529e747885b5f138c8b40807
Johnny P. <dark_skateboarding2@hotmail.com>
2025-12-03 12:04:03 +0200

    fix: main fixes

--- src/gr/aueb/cf/testgit/HelloWorld.java
+++ src/gr/aueb/cf/testgit/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
         System.out.println("Hello Coding");
         System.out.println("Hello Coding Festival!!!!");
         System.out.println("Hello Coding Factory!!!!!");
-
+        System.out.println("Hello Coding Factory 2021!!!!!");
     }
 
 }
