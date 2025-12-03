18bbfb3bfd8b278346ef244de613ea6aaf33e1a5
Johnny P. <dark_skateboarding2@hotmail.com>
2025-12-03 11:53:02 +0200

    feat: add subapp

---
+++ src/gr/aueb/cf/testgit/SubApp.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.testgit;
+
+public class SubApp {
+    public static void main(String[] args) {
+
+    }
+
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+}
